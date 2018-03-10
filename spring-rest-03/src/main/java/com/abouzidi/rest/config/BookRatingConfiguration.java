76c238e8cd66603e927c7211f96779a8019312fa
Afif Bouzidi <afif.bouzidi@gmail.com>
2018-03-10 04:58:50 +0100

    Endpoints Implementation

--- spring-rest-03/src/main/java/com/abouzidi/rest/config/BookRatingConfiguration.java
+++ spring-rest-03/src/main/java/com/abouzidi/rest/config/BookRatingConfiguration.java
@@ -2,9 +2,7 @@ package com.abouzidi.rest.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import com.abouzidi.rest.dto.BookRatingMapper;
-
 import fr.xebia.extras.selma.Selma;
 
 @Configuration
