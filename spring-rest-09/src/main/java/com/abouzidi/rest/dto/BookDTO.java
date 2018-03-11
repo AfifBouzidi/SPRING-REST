f3948ac378bc1e761134465a354e83ab0a8aa46a
Afif Bouzidi <afif.bouzidi@gmail.com>
2018-03-11 05:04:58 +0100

    OAuth 2.0 Provider Implementation

---
+++ spring-rest-09/src/main/java/com/abouzidi/rest/dto/BookDTO.java
@@ -0,0 +1,67 @@
+package com.abouzidi.rest.dto;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class BookDTO {
+
+	private long id;
+
+	private String title;
+
+	private String author;
+
+	@JsonIgnore
+	private Date createdAt = new Date();
+
+	private Collection<RatingDTO> ratings = new ArrayList<>();
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public Collection<RatingDTO> getRatings() {
+		return ratings;
+	}
+
+	public void setRatings(Collection<RatingDTO> ratings) {
+		this.ratings = ratings;
+	}
+
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	@Override
+	public String toString() {
+		return "BookDTO [id=" + id + ", title=" + title + ", author=" + author + ", createdAt=" + createdAt
+				+ ", ratings=" + ratings + "]";
+	}
+}
